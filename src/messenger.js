37132546f0e69201cb614f4fcc22d25044d77b05
Matthew Armstrong <raingerber2@gmail.com>
2019-01-09 20:04:55 -0800

    remove unnecessary parentheses

--- src/messenger.js
+++ src/messenger.js
@@ -1,11 +1,10 @@
 const cmd = process.argv[2]
 const args = process.argv.slice(3)
 
-const id = (
+const id =
   'SHELL__MOCK__ID__' +
   Date.now().toString(36) +
   Math.random().toString(36).slice(2)
-)
 
 // This should keep the process alive to wait for a message
 process.on('message', onMessageReceived)
